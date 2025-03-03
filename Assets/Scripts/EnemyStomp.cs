c77624f60ed369e24c49dbe707d91541aeba6004
CF94 <craig.fahy94@gmail.com>
2025-03-03 01:35:59 +0000

    added enemy scripting

---
+++ Assets/Scripts/EnemyStomp.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EnemyStomp : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Weak Point")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
